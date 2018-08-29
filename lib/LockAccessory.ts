350d1195b5329a06a74a926722aff4cf5fd0bde0
Andrew Schaper <andrew@schaper.io>
2018-08-28 21:45:06 -0700

    Wait for updated state from the API when controlling lock

--- lib/LockAccessory.ts
+++ lib/LockAccessory.ts
@@ -1,8 +1,10 @@
-import { HAP, LockProperties } from './types';
+import * as retry from 'retry';
 
+import { Config } from './Config';
 import { Hap } from './HAP';
 import { Logger } from './HSLogger';
 import { Lock } from './Lock';
+import { HAP, LockProperties } from './types';
 
 export class LockAccessory {
   lock: Lock;
@@ -82,21 +84,60 @@ export class LockAccessory {
 
   setLockState(targetState, callback): void {
     let lockMechanismService = this.getOrCreateLockMechanismService();
+    let shouldSecure = (targetState == Hap.Characteristic.LockCurrentState.SECURED);
 
-    Logger.log(`${targetState ? 'Locking' : 'Unlocking'} ${this.lock.nickname}`);
+    Logger.log(`${targetState ? 'Locking' : 'Unlocking'} ${this.lock.nickname}...`);
 
     this.lock.control(targetState).then(() => {
-      if (targetState == Hap.Characteristic.LockCurrentState.SECURED) {
-        lockMechanismService.setCharacteristic(Hap.Characteristic.LockCurrentState, Hap.Characteristic.LockCurrentState.SECURED);
-      } else {
-        lockMechanismService.setCharacteristic(Hap.Characteristic.LockCurrentState, Hap.Characteristic.LockCurrentState.UNSECURED);
-      }
-
-      callback(null);
+      this.waitForNewState(shouldSecure).then((newState) => {
+        lockMechanismService.setCharacteristic(Hap.Characteristic.LockCurrentState, newState);
+        callback(null);
+      })
+      .catch((err) => {
+        Logger.log(err);
+        callback(err);
+      });
     })
     .catch((err) => {
       Logger.log(err);
       callback(err);
+    });
+  }
+
+  waitForNewState(shouldSecure: boolean): Promise<boolean> {
+    let operation = retry.operation({
+      retries: Config.RETRIES,
+      minTimeout: Config.TIMEOUT,
+      maxTimeout: Config.TIMEOUT
+    });
+
+    return new Promise((resolve, reject) => {
+      operation.attempt((attempt) => {
+        this.lock.getState().then((isSecure) => {
+          Logger.debug(`Secure: ${isSecure}, retry attempt: ${attempt}`);
+
+          if (shouldSecure && isSecure) {
+            Logger.log(this.lock.nickname, 'is locked');
+
+            operation.stop();
+            return resolve(Hap.Characteristic.LockCurrentState.SECURED);
+          }
+
+          if (!shouldSecure && !isSecure) {
+            Logger.log(this.lock.nickname, 'is unlocked');
+
+            operation.stop();
+            return resolve(Hap.Characteristic.LockCurrentState.UNSECURED);
+          }
+
+          if (attempt == Config.RETRIES) {
+            operation.stop();
+            return reject(new Error('Unable to retrieve new lock state from the Sesame API'));
+          }
+
+          operation.retry(new Error());
+        });
+      });
     });
   }
 
