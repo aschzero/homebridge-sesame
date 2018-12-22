80999834908a3bce2ff9eac95c44fdefa17e6b66
Andrew Schaper <andrew@schaper.io>
2018-12-22 15:25:19 -0800

    Reduce polling delay by one second

--- lib/Config.ts
+++ lib/Config.ts
@@ -1,5 +1,5 @@
 export namespace Config {
   export const API_URI = 'https://api.candyhouse.co/public'
   export const MAX_RETRIES = 20;
-  export const DELAY = 2000;
+  export const DELAY = 1000;
 }
